fd89ca52ddd1ce396d121ab30c56911451435777
Elen <petrielen@outlook.com>
2022-01-20 15:38:59 -0300

    feat: Criado arquivo base para oranização do código

--- src/app.py
+++ src/app.py
@@ -9,13 +9,13 @@ def home():
 
 @app.route('/inicio')
 def inicio():
-    return render_template('index.html')
+    return render_template('index.html', pagina_selecionada = 'inicio')
 
 @app.route('/producoes')
 def producoes():
-    return render_template('producoes.html')
+    return render_template('producoes.html', pagina_selecionada = 'producoes')
 
 @app.route('/sobre')
 def sobre():
-    return render_template('sobre.html')
+    return render_template('sobre.html', pagina_selecionada = 'sobre')
 
