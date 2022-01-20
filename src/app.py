7c9ee6dd2a1769981dda605c0a4c410a38765eb9
Elen <petrielen@outlook.com>
2022-01-20 19:51:29 -0300

    fix: Desfeito alterações para teste no deploy do heroku

--- src/app.py
+++ src/app.py
@@ -1,25 +1,23 @@
+import os
 from flask import Flask, app, render_template
-from datetime import date
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
+    return inicio()
+
+@app.route('/inicio')
+def inicio():
     return render_template('index.html', pagina_selecionada = 'inicio')
+
+@app.route('/producoes')
+def producoes():
+    return render_template('producoes.html', pagina_selecionada = 'producoes')
 
 @app.route('/sobre')
 def sobre():
-    idade = calcularIdade(date(1994,1,24))
-    return render_template('sobre.html', idade = idade, pagina_selecionada = 'sobre')
-
-@app.route('/projetos')
-def projetos():
-    return render_template('projetos.html', pagina_selecionada = 'projetos')    
-
-def calcularIdade(dataAniversario):
-    hoje = date.today()
-    idade = hoje.year - dataAniversario.year - ((hoje.month, hoje.day) < (dataAniversario.month, dataAniversario.day))
-    return idade
+    return render_template('sobre.html', pagina_selecionada = 'sobre')
 
 
 if __name__ == '__main__':
